ae2088f6dad28b9a483f53e57b323fbd693d122b
Natan200-2 <natanboas21@outlook.com>
2024-02-14 13:16:20 -0300

    componente Sorteio criado e testado

---
+++ src/pages/Sorteio/Sorteio.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react"
+import { RecoilRoot } from "recoil"
+import { useListaParticipantes } from "../../state/hook/useListaParticipantes"
+import Sorteio from "./Sorteio"
+
+jest.mock('../../state/hook/useListaParticipantes', () => {
+    return{
+        useListaParticipantes: jest.fn()
+    }
+})
+
+describe('na página de sorteio', () => {
+    const participantes = [
+        'Teste1',
+        'Teste2',
+        'Teste3'
+    ]
+
+    beforeEach(() => {
+        (useListaParticipantes as jest.Mock).mockReturnValue(participantes);
+    })
+
+    test('todos os participantes podem exibir o seu amigo secreto', () => {
+        render(<RecoilRoot>
+            <Sorteio />
+        </RecoilRoot>)
+
+        const opcoes = screen.queryAllByRole('option');
+        expect(opcoes).toHaveLength(participantes.length);
+    })
+
+})
+
